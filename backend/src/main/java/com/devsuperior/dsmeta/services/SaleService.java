04a6e634efde3d4401bc5df5acdcfb3b92ab9c97
Jefferson Batista <jefferson_tecnico@hotmail.com>
2022-07-14 10:07:41 -0300

    sales controller component - End API

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,5 +1,21 @@
 package com.devsuperior.dsmeta.services;
 
-public class SaleService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.respositories.SaleRepository;
+
+//Componente responsável por executar funções no Banco de Dados
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
 }
